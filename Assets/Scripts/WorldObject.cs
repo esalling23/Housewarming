ae1f9770dc47194e6c0afccbdeb970274a5622cd
esalling23 <esalling23@gmail.com>
2021-07-14 21:31:39 -0400

    Adds world object hover effects

--- Assets/Scripts/WorldObject.cs
+++ Assets/Scripts/WorldObject.cs
@@ -4,18 +4,47 @@ using UnityEngine;
 
 public class WorldObject : MonoBehaviour
 {
+    // Rotation support on hover
+    [SerializeField] GameObject _childSprite;
+    Transform _childTransform;
+    int _rotateAngle = 15;
+    Vector3 _rotatePivot;
+    // Used in determining rotation pivot
+    BoxCollider2D _collider;
+    float _halfHeight;
+    float _halfWidth;
+    SpriteRenderer _spriteRenderer;
 
-    // Start is called before the first frame update
+
+    void Awake()
+    {
+        _spriteRenderer = _childSprite.GetComponent<SpriteRenderer>();
+        _collider = GetComponent<BoxCollider2D>();
+        _childTransform = _childSprite.transform;
+    }
+
     void Start()
     {
-
+        _halfWidth = _collider.size.x / 2;
+        _halfHeight = _collider.size.y / 2;
     }
-
-    // Update is called once per frame
-    void Update()
+    void OnMouseEnter()
     {
+        Debug.Log("Mouse entering");
 
+        _rotatePivot = new Vector3(_childTransform.position.x - _halfWidth, _childTransform.position.y - _halfHeight, 0);
+        _spriteRenderer.color = new Color(0.9f, 0.9f, 0.9f, 1);
+        _childTransform.RotateAround(_rotatePivot, Vector3.forward, _rotateAngle);
     }
 
+    void OnMouseExit()
+    {
+        _spriteRenderer.color = Color.white;
+        _childTransform.RotateAround(_rotatePivot, Vector3.forward, -_rotateAngle);
+    }
 
+    void OnMouseDown()
+    {
+        _childTransform.Rotate(0, 0, 0.25f);
+    }
 }
