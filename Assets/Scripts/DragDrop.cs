455457d76d2cdea22c8229b9a4fb15dc81d02adb
esalling23 <esalling23@gmail.com>
2021-07-16 17:42:24 -0400

    Move all drag/drop functionality to DragDrop script

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -4,24 +4,76 @@ using UnityEngine;
 
 public class DragDrop : MonoBehaviour
 {
-    bool _isDragging = false;
+    #region Fields
+    // Caching built-ins
     Transform _transform;
     Camera _mainCamera;
-    BoxCollider2D _collider;
+
+    // Drag support
+    bool _isDragging = false;
     Vector2 _originalPos;
 
-    public bool IsDragging { get { return _isDragging; } }
+    // Drag/hover rotation pivot support
+    bool _isHoverRotated = false;
+    int _rotateAngle = 15;
+    BoxCollider2D _collider;
+    float _halfHeight;
+    float _halfWidth;
+    [SerializeField] GameObject _childObj;
+    Transform _childTransform;
+    Vector3 _rotatePivot;
+
+    // Color change support
+    SpriteRenderer _childSprite;
+    Color _hoverColor = new Color(0.9f, 0.9f, 0.9f, 1);
+
+    #endregion
+
+    #region Properties
+
+    Vector3 RotationPivot
+    {
+        get
+        {
+            _rotatePivot.x = transform.position.x - _halfWidth;
+            _rotatePivot.y = transform.position.y - _halfHeight;
+            return _rotatePivot;
+        }
+    }
+
+    bool ShouldHandleMouseExit
+    {
+        get
+        {
+            return _isHoverRotated && !IsDragging;
+        }
+    }
+
+    bool IsDragging { get { return _isDragging; } }
+
+    #endregion
+
+    #region Methods
 
     void Awake()
     {
         _transform = transform;
         _mainCamera = Camera.main;
         _collider = GetComponent<BoxCollider2D>();
+
+        _childTransform = _childObj.transform;
+        _childSprite = _childObj.GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        _halfWidth = _collider.size.x / 2;
+        _halfHeight = _collider.size.y / 2;
     }
 
     void Update()
     {
-        if (_isDragging)
+        if (IsDragging)
         {
             Vector2 mousePos = _mainCamera.ScreenToWorldPoint(Input.mousePosition) - _transform.position;
             // Rounding position forces grid-snap
@@ -52,9 +104,14 @@ public class DragDrop : MonoBehaviour
         if (!CanPlace())
         {
             _transform.position = _originalPos;
+            HandleMouseExit();
         }
 
-        Debug.Log("OnMouseUp");
+        if (ShouldHandleMouseExit)
+        {
+            HandleMouseExit();
+        }
+
         _isDragging = false;
         _collider.isTrigger = false;
     }
@@ -63,8 +120,43 @@ public class DragDrop : MonoBehaviour
     {
         _originalPos = new Vector2(_transform.position.x, _transform.position.y);
 
-        Debug.Log("OnMouseDown");
         _isDragging = true;
         _collider.isTrigger = true;
     }
+
+    void OnMouseExit()
+    {
+        if (ShouldHandleMouseExit)
+        {
+            HandleMouseExit();
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        if (!IsDragging)
+        {
+            HandleMouseEnter();
+        }
+    }
+
+    void HandleMouseEnter()
+    {
+        _isHoverRotated = true;
+        _childTransform.RotateAround(RotationPivot, Vector3.forward, _rotateAngle);
+
+        _childSprite.color = _hoverColor;
+    }
+
+    void HandleMouseExit()
+    {
+        Debug.Log("Handling Exit");
+        _isHoverRotated = false;
+        // Reset rotate
+        _childTransform.RotateAround(RotationPivot, Vector3.forward, -_rotateAngle);
+
+        // Reset color
+        _childSprite.color = Color.white;
+    }
+    #endregion
 }
--- Assets/Scripts/WorldObject.cs
+++ Assets/Scripts/WorldObject.cs
@@ -5,110 +5,19 @@ using UnityEngine;
 public class WorldObject : MonoBehaviour
 {
     #region Fields
-    // Rotation support on hover
-    Vector3 _startingPos;
-    DragDrop _dragDrop;
-    [SerializeField] GameObject _childObj;
-    Transform _childTransform;
-    int _rotateAngle = 15;
-    Vector3 _rotatePivot;
 
-
-    // Used in determining rotation pivot
-    BoxCollider2D _collider;
-    float _halfHeight;
-    float _halfWidth;
-
-    // Hover effect support
-    SpriteRenderer _childSprite;
-    Color _hoverColor = new Color(0.9f, 0.9f, 0.9f, 1);
-
-    #endregion
-
-    #region Properties
-    Vector3 RotationPivot
-    {
-        get
-        {
-            _rotatePivot.x = transform.position.x - _halfWidth;
-            _rotatePivot.y = transform.position.y - _halfHeight;
-            return _rotatePivot;
-        }
-    }
-
-    bool IsNormalRotation
-    {
-        get
-        {
-            return _childTransform.localRotation.z == 0;
-        }
-    }
-
-    bool ShouldHandleMouseEvent
-    {
-        get
-        {
-            return !IsNormalRotation && !_dragDrop.IsDragging;
-        }
-    }
+    // Object details for display
+    public string name;
 
     #endregion
 
     #region Methods
-    void Awake()
-    {
-        _dragDrop = GetComponent<DragDrop>();
-        _collider = GetComponent<BoxCollider2D>();
 
-        _childTransform = _childObj.transform;
-        _childSprite = _childObj.GetComponent<SpriteRenderer>();
-    }
-
-    void Start()
-    {
-        _startingPos = Vector3.zero;
-        _halfWidth = _collider.size.x / 2;
-        _halfHeight = _collider.size.y / 2;
-    }
-
-    void OnMouseEnter()
-    {
-        if (!_dragDrop.IsDragging)
-        {
-            HandleMouseEnter();
-        }
-    }
-
-    void OnMouseExit()
-    {
-        if (ShouldHandleMouseEvent)
-        {
-            HandleMouseExit();
-        }
-    }
-
-    void OnMouseUp()
-    {
-        if (ShouldHandleMouseEvent)
-        {
-            HandleMouseExit();
-        }
-    }
-
-    void HandleMouseEnter()
-    {
-        _childTransform.RotateAround(RotationPivot, Vector3.forward, _rotateAngle);
-
-        _childSprite.color = _hoverColor;
-    }
-
-    void HandleMouseExit()
-    {
-        // Reset rotate
-        _childTransform.RotateAround(RotationPivot, Vector3.forward, -_rotateAngle);
-
-        // Reset color
-        _childSprite.color = Color.white;
+    void OnMouseDown() {
+        EventManager.TriggerEvent(EventName.SelectObject, new Dictionary<string, object> {
+            { "name", name },
+            { "obj", gameObject }
+        });
     }
 
     #endregion
