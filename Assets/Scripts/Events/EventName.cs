aa957bc7b3c4a5ba60858424977bdbb199f17d8f
esalling23 <esalling23@gmail.com>
2021-07-19 23:37:04 -0400

    Adds new event names for gameplay

--- Assets/Scripts/Events/EventName.cs
+++ Assets/Scripts/Events/EventName.cs
@@ -2,9 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Event names for event management
+/// All events are listed here
+/// </summary>
 public enum EventName
 {
     SelectObject,
     RotateObject,
-    CycleObject
+    CycleObject,
+    StartDialogue,
+    CompleteDialogue,
+    StartArea,
+    CompleteArea,
+    CompletePhase,
+    GameOver
 }
