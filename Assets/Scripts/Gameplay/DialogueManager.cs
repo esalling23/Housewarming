1fd1eebef550e20e1792e6bd6cc11c44423b9eac
esalling23 <esalling23@gmail.com>
2021-07-23 12:02:55 -0400

    Cleans up DialogueManager
    - Adds comments, regions
    - Moved typewriter component to DialogueView object in scene, updates reference in script
    - Moves phase variable assignment to start of phase only
    - Bug fix: Shows object in scene before invoking coroutine

--- Assets/Scripts/Gameplay/DialogueManager.cs
+++ Assets/Scripts/Gameplay/DialogueManager.cs
@@ -5,13 +5,21 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Manages the dialogue view - displaying dialogue for user
+/// </summary>
 public class DialogueManager : MonoBehaviour
 {
+    #region Fields
     GameObject _dialogueView;
     Text _dialogueText;
     DialoguePhase _currentDialoguePhase;
     int _dialogueCounter;
     Typewriter _typewriter;
+
+    #endregion
+
+    #region Methods
 
     void Start()
     {
@@ -26,54 +34,68 @@ public class DialogueManager : MonoBehaviour
     {
         _dialogueView = GameObject.FindWithTag("DialogueView");
         _dialogueText = GameObject.FindWithTag("DialogueText").GetComponent<Text>();
-        _typewriter = GetComponent<Typewriter>();
+        _typewriter = _dialogueView.GetComponent<Typewriter>();
     }
 
-    void Update()
-    {
-
-    }
-
+    /// <summary>
+    /// Setup to start dialogue
+    /// - Resets counter
+    /// - Gets current dialogue phase
+    /// - Shows view and writes first dialogue
+    /// </summary>
+    /// <param name="msg">null</param>
     void StartDialogue(Dictionary<string, object> msg)
     {
         // Debug.Log("Starting dialogue");
         _dialogueCounter = 0;
+        _currentDialoguePhase = GameManager.Instance.CurrentPhase.dialoguePhase;
 
-        SetDialogue();
+        // Must show dialogue view before setting dialogue
         ShowDialogueView();
+        WriteDialogue();
     }
 
+    /// <summary>
+    /// Handles moving to the next piece of dialogue or triggering the end
+    /// </summary>
     void ContinueDialogue()
     {
         if (_dialogueCounter < _currentDialoguePhase.dialogues.Count() - 1)
         {
             _dialogueCounter++;
-            SetDialogue();
+            WriteDialogue();
         }
         else
         {
-            _dialogueCounter = 0;
             _dialogueView.SetActive(false);
             EventManager.TriggerEvent(EventName.CompleteDialogue, null);
         }
     }
 
-    void SetDialogue()
+    /// <summary>
+    /// Resets text and displays the current piece of text
+    /// </summary>
+    void WriteDialogue()
     {
-        // Debug.Log("Setting Dialogue Phase");
-        _currentDialoguePhase = GameManager.Instance.CurrentPhase.dialoguePhase;
-
         _typewriter.Write(_currentDialoguePhase.dialogues[_dialogueCounter], _dialogueText);
     }
 
+    /// <summary>
+    /// Displays the dialogue view in the scene
+    /// </summary>
     void ShowDialogueView()
     {
         // To do - animate in?
         _dialogueView.SetActive(true);
     }
 
+    /// <summary>
+    /// Button click handler set in inspector - continues dialogue
+    /// </summary>
     public void HandleContinueDialogueBtnClick()
     {
         ContinueDialogue();
     }
+
+    #endregion
 }
